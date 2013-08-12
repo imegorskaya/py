35b264f4c74cfce4bd0329859834f7e4d397f907
Irina Megorskaya <irina.megorskaya@gmail.com>
2013-08-12 17:58:35 +0400

    adding new file to my repo

---
+++ MySimplePythonApplication/misc/Solver.py
@@ -0,0 +1,30 @@
+__author__ = 'wombat'
+__project__ = 'MySimplePythonApplication'
+
+import math
+
+
+class Solver:
+
+    a=1
+    b=2
+    c=3
+    def calculate(self,a,b,c):
+
+
+        d = b ** 2 - 4 * a * c
+        if d>=0:
+            disc = math.sqrt(d)
+        else:
+            print ("error")
+        root1 = (-b + disc) / (2 * a)
+        root2 = (-b - disc) / (2 * a)
+        print(root1, root2)
+
+class MyPythonFile():
+    def doSmth(self):
+        pass
+
+Solver().calculate(6,2,4)
+
+ 
